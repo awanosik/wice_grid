b5f6eb635a3f68b155649d606c461a7667d1c54c
Andrew Wanosik <awanosik@chepri.com>
2015-07-01 21:02:31 -0400

    Javascript fix for auto submit

--- vendor/assets/javascripts/wice_grid.js
+++ vendor/assets/javascripts/wice_grid.js
@@ -1,11 +1,11 @@
 
 $(function(){
-	$('input[type=text]').on('keyup', function(e) {
+	$('.wice_grid_form input[type=text]').on('keyup', function(e) {
 		if (e.which == 13) {
 			$(this).closest('form').submit();	
 		}
 	});
-	$('input:not([type="text"]), select').change(function(){
+	$('.wice_grid_form input:not([type="text"]), select').change(function(){
 		$(this).closest('form').submit();
 	});
 	$('.datetimepicker').each(function(){
